1c2526e6b0fd9d32dfcea16c76f89323fdb10dd0
Irina Zolina <zolina.iris@ya.ru>
2022-08-25 14:09:49 +0300

    refactoring removing unused code

--- src/test/java/github/SelenideRepository.java
+++ src/test/java/github/SelenideRepository.java
@@ -1,6 +1,5 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -10,18 +9,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideRepository {
-
-  @Test
-  void shouldFindSelenideRepository(){
-    // открыть страницу github.com
-    open("https://github.com");
-    // ввести в поле поиска selenide и нажать Enter
-    $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-    // нажимаем на линк от первого результата поиска
-    $$("ul.repo-list li").first().$("a").click();
-    // check: в заголовке встрачается selenide/selenide
-    $("#repository-container-header").shouldHave(text("selenide / selenide"));
-  }
 
   @Test
   void shouldFindSoftAssertions(){
@@ -36,7 +23,6 @@ public class SelenideRepository {
 
     // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
     $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
-    //$("[data-filterable-for=wiki-pages-filter]").$(".details-reset").shouldHave(text("SoftAssertions"));
 
     // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
     $(byText("SoftAssertions")).click();
